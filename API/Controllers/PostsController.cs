8fb500628e35804e4857609cfe2be4eae58e171d
Mike Hunsicker <hunsickm@matc.edu>
2024-10-06 12:27:22 -0500

    Added endpoint to return a single post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -15,11 +15,30 @@ namespace API.Controllers
             this._context = context;
         }
 
-        // GET api/posts
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns> A list of posts</returns>     
         [HttpGet(Name = "GetPosts")]
         public ActionResult<List<Post>> Get()
         {
             return this._context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// Get api/post/[id]
+        /// </summary>
+        /// <param name="id">post id</param>
+        /// <returns>A single post</returns>
+        public ActionResult<Post> GetById(Guid id)
+        {
+          var post = this._context.Posts.Find(id);
+          if (post is null)
+          {
+            return NotFound();
+          }
+
+          return Ok(post);
         }
     }
 }
