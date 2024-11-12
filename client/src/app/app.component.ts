3be5cec6a5ef47e6b08ff369e7c709ff5c15cbd3
scott wegmann <wegmanns@gmatc.matc.edu>
2024-11-11 20:52:37 -0600

    Completed Activity 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet, 
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'client';
-  posts: any;
-  
-  constructor(private http: HttpClient) {
-
-  }
-
-  ngOnInit(): void {
-    this.http.get('http://localhost:5289/api/posts').subscribe({
-      next: (response) => this.posts = response,
-      error: (e) => console.error(e),
-      complete: () => console.log('complete')
-    })
-  }
+export class AppComponent {
+ title = 'Blogbox';
 }
+  
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent } from './home/home.component';
+import {CreatePostComponent } from './create-post/create-post.component';
+import {ViewPostComponent} from './view-post/view-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'create', component: CreatePostComponent },
+    {path: 'post/:id', component: ViewPostComponent},
+    {path: '**', component: HomeComponent, pathMatch: 'full'}
+];
